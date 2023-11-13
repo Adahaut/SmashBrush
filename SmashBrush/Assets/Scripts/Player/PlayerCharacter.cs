55a3d37913d90e919e61a6af72f14af557f9e7e0
Adahaut <adao.vo.t@gmail.com>
2023-11-13 14:05:36 +0100

    fix bugs

--- SmashBrush/Assets/Scripts/Player/PlayerCharacter.cs
+++ SmashBrush/Assets/Scripts/Player/PlayerCharacter.cs
@@ -14,12 +14,17 @@ public class PlayerCharacter : MonoBehaviour
 
     public PlayerController _controller;
 
-    public GameObject _UI;
+    public TextMeshProUGUI _UI;
     private void Awake()
     {
         _controller = GetComponent<PlayerController>();
         _myTransform = transform;
         CreateUI();
+    }
+
+    private void Update()
+    {
+        _UI.text = "Player " + Camera.main.GetComponent<CameraMovement>()._nbPlayer + " " + _percent + " %";
     }
 
     private void CreateUI()
@@ -29,34 +34,27 @@ public class PlayerCharacter : MonoBehaviour
         {
             _UI.GetComponent<RectTransform>().anchoredPosition = new Vector3(-835, 495, 0);
             Camera.main.GetComponent<CameraMovement>()._nbPlayer = 1;
-            return;
         }
         else if (Camera.main.GetComponent<CameraMovement>()._nbPlayer == 1)
         {
             _UI.GetComponent<RectTransform>().anchoredPosition = new Vector3(890, 495, 0);
             Camera.main.GetComponent<CameraMovement>()._nbPlayer = 2;
-            return;
         }
         else if (Camera.main.GetComponent<CameraMovement>()._nbPlayer == 2)
         {
             _UI.GetComponent<RectTransform>().anchoredPosition = new Vector3(-835, -495, 0);
             Camera.main.GetComponent<CameraMovement>()._nbPlayer = 3;
-            return;
         }
         else if (Camera.main.GetComponent<CameraMovement>()._nbPlayer == 3)
         {
             _UI.GetComponent<RectTransform>().anchoredPosition = new Vector3(890, -495, 0);
             Camera.main.GetComponent<CameraMovement>()._nbPlayer = 4;
-            return;
         }
-        _UI.GetComponent<TextMeshProUGUI>().text = "Player " + Camera.main.GetComponent<CameraMovement>()._nbPlayer;
+        _UI.text = "Player " + Camera.main.GetComponent<CameraMovement>()._nbPlayer + " " + _percent + " %";
 
     }
 
-    private void Update()
-    {
-        _UI.GetComponent<TextMeshProUGUI>().text = "Player " + Camera.main.GetComponent<CameraMovement>()._nbPlayer;
-    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.gameObject.tag == "WorldEdges")
