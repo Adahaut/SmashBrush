b0fb73cd0e53d61537e4f980b0ac6b63c8888e0c
Adahaut <adao.vo.t@gmail.com>
2023-11-13 13:41:08 +0100

    UI basics

--- SmashBrush/Assets/Scripts/Player/PlayerCharacter.cs
+++ SmashBrush/Assets/Scripts/Player/PlayerCharacter.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -13,12 +14,49 @@ public class PlayerCharacter : MonoBehaviour
 
     public PlayerController _controller;
 
+    public GameObject _UI;
     private void Awake()
     {
         _controller = GetComponent<PlayerController>();
         _myTransform = transform;
+        CreateUI();
     }
 
+    private void CreateUI()
+    {
+        _UI = Instantiate(_UI, GameObject.Find("Canvas").transform);
+        if (Camera.main.GetComponent<CameraMovement>()._nbPlayer == 0)
+        {
+            _UI.GetComponent<RectTransform>().anchoredPosition = new Vector3(-835, 495, 0);
+            Camera.main.GetComponent<CameraMovement>()._nbPlayer = 1;
+            return;
+        }
+        else if (Camera.main.GetComponent<CameraMovement>()._nbPlayer == 1)
+        {
+            _UI.GetComponent<RectTransform>().anchoredPosition = new Vector3(890, 495, 0);
+            Camera.main.GetComponent<CameraMovement>()._nbPlayer = 2;
+            return;
+        }
+        else if (Camera.main.GetComponent<CameraMovement>()._nbPlayer == 2)
+        {
+            _UI.GetComponent<RectTransform>().anchoredPosition = new Vector3(-835, -495, 0);
+            Camera.main.GetComponent<CameraMovement>()._nbPlayer = 3;
+            return;
+        }
+        else if (Camera.main.GetComponent<CameraMovement>()._nbPlayer == 3)
+        {
+            _UI.GetComponent<RectTransform>().anchoredPosition = new Vector3(890, -495, 0);
+            Camera.main.GetComponent<CameraMovement>()._nbPlayer = 4;
+            return;
+        }
+        _UI.GetComponent<TextMeshProUGUI>().text = "Player " + Camera.main.GetComponent<CameraMovement>()._nbPlayer;
+
+    }
+
+    private void Update()
+    {
+        _UI.GetComponent<TextMeshProUGUI>().text = "Player " + Camera.main.GetComponent<CameraMovement>()._nbPlayer;
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.gameObject.tag == "WorldEdges")
