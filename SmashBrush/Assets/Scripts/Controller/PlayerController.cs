6596ea86fa464cd726106d0ff531d9cd09488dda
Boushi33 <A.JOURDANNAUD@lahorde.school>
2023-11-14 16:11:52 +0100

    Added 3D model with rudimentary animations

--- SmashBrush/Assets/Scripts/Controller/PlayerController.cs
+++ SmashBrush/Assets/Scripts/Controller/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private GameObject _atkVisual;
     private PlayerMovement _playerMovement;
+    private Animator _animator;
 
     private Transform _myTransform;
     private float _coolDown;
@@ -18,6 +19,7 @@ public class PlayerController : MonoBehaviour
     {
         _playerMovement = GetComponent<PlayerMovement>();
         _myTransform = _playerMovement.GetComponentInParent<Transform>();
+        _animator = GetComponentInChildren<Animator>();
         _coolDown = 0f;
         _isStun = false;
 
@@ -75,10 +77,12 @@ public class PlayerController : MonoBehaviour
                 fistPunch.Execute();
                 _coolDown = Time.time + fistPunch.GetSpeed();
 
-                _atkVisual.SetActive(true);
-                _atkVisual.transform.position = new Vector3(_playerMovement._isFacingLeft ? _myTransform.position.x - fistPunch.GetRange() / 2 : _myTransform.position.x + fistPunch.GetRange() / 2, _myTransform.position.y, _myTransform.position.z);
-                _atkVisual.transform.localScale = new Vector3(fistPunch.GetRange(), 0.2f, 0.2f);
-                StartCoroutine(AttackAnimation(fistPunch.GetSpeed()));
+                _animator.SetTrigger("Punch");
+
+                //_atkVisual.SetActive(true);
+                //_atkVisual.transform.position = new Vector3(_playerMovement._isFacingLeft ? _myTransform.position.x - fistPunch.GetRange() / 2 : _myTransform.position.x + fistPunch.GetRange() / 2, _myTransform.position.y, _myTransform.position.z);
+                //_atkVisual.transform.localScale = new Vector3(fistPunch.GetRange(), 0.2f, 0.2f);
+                //StartCoroutine(AttackAnimation(fistPunch.GetSpeed()));
             }
         }
 
@@ -95,10 +99,12 @@ public class PlayerController : MonoBehaviour
                 feetPunch.Execute();
                 _coolDown = Time.time + feetPunch.GetSpeed();
 
-                _atkVisual.SetActive(true);
-                _atkVisual.transform.position = new Vector3(_playerMovement._isFacingLeft ? _myTransform.position.x -feetPunch.GetRange() / 2 : _myTransform.position.x + feetPunch.GetRange() / 2, _myTransform.position.y, _myTransform.position.z);
-                _atkVisual.transform.localScale = new Vector3(feetPunch.GetRange(), 0.2f, 0.2f);
-                StartCoroutine(AttackAnimation(feetPunch.GetSpeed()));
+                _animator.SetTrigger("Kick");
+
+                //_atkVisual.SetActive(true);
+                //_atkVisual.transform.position = new Vector3(_playerMovement._isFacingLeft ? _myTransform.position.x -feetPunch.GetRange() / 2 : _myTransform.position.x + feetPunch.GetRange() / 2, _myTransform.position.y, _myTransform.position.z);
+                //_atkVisual.transform.localScale = new Vector3(feetPunch.GetRange(), 0.2f, 0.2f);
+                //StartCoroutine(AttackAnimation(feetPunch.GetSpeed()));
             }
         }
 
